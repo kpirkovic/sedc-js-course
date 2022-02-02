555db5f55e83c65743c153df103e4accdfdb239c
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-02 19:44:35 +0100

    Fixed some typo

--- Class Exercise 02/script.js
+++ Class Exercise 02/script.js
@@ -8,12 +8,12 @@ function showSideBar() {
         document.getElementById("side-bar").style.width = "12rem"
         document.getElementById("side-bar").style.padding = "1.5rem 2rem"
         document.getElementById("main-section").style.marginLeft = "12.5rem"
-        document.getElementById("show-hide-btn").innerText = "Show"
+        document.getElementById("show-hide-btn").innerText = "Hide"
     } else if(clicked == 2){
         document.getElementById("side-bar").style.width = "0"
         document.getElementById("side-bar").style.padding = "0"
         document.getElementById("main-section").style.marginLeft = "0"
-        document.getElementById("show-hide-btn").innerText = "Hide"
+        document.getElementById("show-hide-btn").innerText = "Show"
         clicked = 0;
     }
 }
