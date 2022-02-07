d72d619652dba1250db46db0929f0f092ec5b12c
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-07 18:04:45 +0100

    Added a js function into the js script file

---
+++ Class Exercise 03/script.js
@@ -0,0 +1,17 @@
+function changeBg() {
+    let dropDownBg = document.getElementById("bg-color-picker").value;
+
+    if(dropDownBg == "white"){
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+    } else if (dropDownBg == "black") {
+        document.body.style.background = "#1b1717";
+        document.body.style.color = "white";
+    } else if (dropDownBg == "navy") {
+        document.body.style.background = "#1a374b";
+        document.body.style.color = "white";
+    } else if (dropDownBg == "orange") {
+        document.body.style.background = "#ff9a49";
+        document.body.style.color = "black";
+    }
+}
