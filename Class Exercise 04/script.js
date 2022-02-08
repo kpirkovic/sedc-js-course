0168a8e7e62c1ee96c6079551d8ec808f0d5db43
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-08 17:18:36 +0100

    Added some animations on the popup modal

--- Class Exercise 04/script.js
+++ Class Exercise 04/script.js
@@ -1,6 +1,8 @@
 function openModal (){
-    document.getElementById("pop-up-modal").style.display = "flex";
+    document.getElementById("pop-up-modal").classList.add("show")
+    document.getElementById("pop-up-form").classList.add("show")
 }
 function closeModal () {
-    document.getElementById("pop-up-modal").style.display = "none";
+    document.getElementById("pop-up-modal").classList.remove("show")
+    document.getElementById("pop-up-form").classList.remove("show")
 }
