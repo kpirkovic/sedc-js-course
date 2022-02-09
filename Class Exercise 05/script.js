603843193041c3b0f8197c5d8e7724fb8b8fe578
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-09 18:50:22 +0100

    Added a function to Add a To Do

---
+++ Class Exercise 05/script.js
@@ -0,0 +1,15 @@
+function addToDo () {
+    let myGoals = document.getElementById("goals-input").value;
+    if(myGoals === "") 
+    {
+       alert("Write a goal before you click the Add Button")
+    } else {
+        document.getElementById("input-wraper").innerHTML += 
+        "<div id='todo-wraper'>"
+        + "<button id='complete-btn'></button>" 
+        + "<span>" + myGoals + "</span>" 
+        + "</div>"
+
+        document.getElementById("goals-input").value = "";
+    }
+}
