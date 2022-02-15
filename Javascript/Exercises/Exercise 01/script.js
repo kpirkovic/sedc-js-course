50698ec4496824d67f57e21993961c3e144bcaf7
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-15 20:17:39 +0100

    Created an inputs for my meeters calculator and rectangle area calculator

--- Javascript/Exercises/Exercise 01/script.js
+++ Javascript/Exercises/Exercise 01/script.js
@@ -1,4 +1,21 @@
-let feet = 50;
 
-let result = feet * 0.3048;
-console.log(feet + " feet" + " is " + result + " meters");
+function meetersConverter(){
+    const feet = document.getElementById("feet").value;
+    const meeters = document.getElementById("results-meters");
+    const result = feet * 0.3048;
+
+    meeters.textContent = "Meeters = " + result;
+    document.getElementById("feet").value = "";
+}
+
+function calculateArea(){
+    const printArea = document.getElementById("area")
+    const printPerimetar = document.getElementById("perimetar")
+    const width = document.getElementById("width").value;
+    const height = document.getElementById("height").value;
+
+    const area = width * height;
+
+    printArea.textContent = "Area = " + area;
+    document.getElementById("area").value = "";
+}
