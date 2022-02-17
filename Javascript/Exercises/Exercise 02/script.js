201227d87c1060f6d0acc80a48023b7f0b1cad95
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-17 19:02:30 +0100

    Finished the second exercise with if statments

--- Javascript/Exercises/Exercise 02/script.js
+++ Javascript/Exercises/Exercise 02/script.js
@@ -1,3 +1,4 @@
+//Functions Exercise
 function celsiusToFahrenheit(temp) {
     let temperature = temp * 9 / 5 + 32;
     return temperature;
@@ -14,3 +15,18 @@ function fahrenheitToCelsius(temp) {
 let fahrenheit = 102.2;
 let fahrenheitCalculator = fahrenheitToCelsius(fahrenheit);
 console.log(`NN°F ${fahrenheit} is ${fahrenheitCalculator} NN°C`);
+
+
+//Inputs Exercise
+
+let fridayCash = 10;
+
+if (fridayCash >= 50) {
+    console.log(`You should go out for dinner and movie`);
+} else if (fridayCash  >=35) {
+    console.log(`You should go out for dinner and movie`);
+} else if (fridayCash  >=12) {
+    console.log(`You should see a movie`);
+} else {
+    console.log(`Looks like you'll be watching TV`);
+}
