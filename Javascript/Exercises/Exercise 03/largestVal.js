edcb8df3cb3f1d13d04fe24dba6ff0b6d200c170
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-22 20:00:57 +0100

    Checking even and uneven numbers in a list

---
+++ Javascript/Exercises/Exercise 03/largestVal.js
@@ -0,0 +1,43 @@
+/* let counter = 0;
+
+let values = [];
+
+while (counter < 4){
+    let insertedValue = prompt(`Vnesi Broj na pozicija ${counter}:`);
+    values.push(insertedValue);
+    counter++;
+}
+
+console.log(values);
+ */
+let number = [10,50,78,1500,447,1000];
+let maxNumber = 0;
+
+let i = 0;
+while(i < number.length){
+    if(maxNumber < number[i]){
+        maxNumber = number[i];
+    }
+    i++;
+}
+
+console.log(`The largest number in the array is ${maxNumber}`);
+
+let days = [":", 2, 3, 4, 5, 6, 7];
+
+for(let i = 0; i < days.length; i++){
+    if((days.length-1) == i){
+        console.log(days[i]);
+    }else {
+        console.log(days[i] + ", ");
+    }
+}
+
+let = myNumbersList = [5,3,85,53,21,45,84,22,35,44];
+for(let i = 0; i < myNumbersList.length; i++){
+    if(myNumbersList[i] % 2 == 0) {
+        document.write(`${myNumbersList[i]} Is Even <br>`);
+    } else {
+        document.write(`${myNumbersList[i]} Is Not Even <br>`);
+    }
+}
