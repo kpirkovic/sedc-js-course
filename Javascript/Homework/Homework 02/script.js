747cb5d0bdba83b0eb6fd1036ce23c05bcc7ff66
kpirkovic <kristijanpirkovic@gmail.com>
2022-02-23 22:33:09 +0100

    Finished Bonus Homework

--- Javascript/Homework/Homework 02/script.js
+++ Javascript/Homework/Homework 02/script.js
@@ -1,3 +1,4 @@
+//First Homework
 function findMySymbol (){
     let count = 0;
     let foundSymbols = "";
@@ -23,6 +24,32 @@ function findMySymbol (){
     document.getElementById('my-input').value = "";
 }
 
+//Bonus Exercise
 
+let userInputs;
+let evenSum = 0;
+let unevenSum = 0;
+let displayEven = document.getElementById('even-num');
+let displayUneven = document.getElementById('uneven-num');
 
+for(let i = 0; i < 10; i++){
+    userInputs = parseFloat(prompt(`Random number here (input ${i+1} of 10)`));
+    if(userInputs % 2 === 0){
+        evenSum += userInputs;     
+        displayEven.textContent += userInputs + " + ";
+    } else {
+        unevenSum += userInputs;
+        displayUneven.textContent += userInputs + " + ";
+    }
+}
 
+displayEven.textContent += " = " + evenSum;
+displayUneven.textContent += " = " + unevenSum;
+
+if(evenSum > unevenSum) {
+    document.write(`${evenSum} is bigger then ${unevenSum}`);
+} else if (evenSum < unevenSum) {
+    document.write(`${unevenSum} is bigger then ${evenSum}`);
+} else {
+    document.write(`They are equal or invalid`);
+}
