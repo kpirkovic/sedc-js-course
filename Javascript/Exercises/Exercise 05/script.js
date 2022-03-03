ff0b17df2fcfce9993ac1549d83a2bacca4d8800
kpirkovic <kristijanpirkovic@gmail.com>
2022-03-03 19:28:30 +0100

    Class exercise with events

---
+++ Javascript/Exercises/Exercise 05/script.js
@@ -0,0 +1,8 @@
+const myBtn = document.getElementById('my-btn');
+const myParagraph = document.getElementById('my-paragraph');
+
+myBtn.addEventListener('click', ()=> {
+    myParagraph.style.fontSize = '22px';
+    myParagraph.style.fontFamily = '"Poppins", sans-serif';
+    myParagraph.style.color = 'orange';
+})
