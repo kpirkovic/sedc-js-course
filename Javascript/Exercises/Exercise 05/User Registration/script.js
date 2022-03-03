bee5e840cd0c9d00cf12df2bdc34569abf42cf3c
kpirkovic <kristijanpirkovic@gmail.com>
2022-03-03 22:07:05 +0100

    Finished first Homework with User Registration

--- Javascript/Exercises/Exercise 05/User Registration/script.js
+++ Javascript/Exercises/Exercise 05/User Registration/script.js
@@ -3,15 +3,34 @@ const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const submit = document.getElementById('submit');
+const error = document.getElementById('erorr');
 
-const myUL = document.getElementById('my-ul');
+const  myTable = document.getElementById('my-table');
+const  tableHead = myTable.firstElementChild.firstElementChild;
+const  tableBody = myTable.lastElementChild.firstElementChild;
 
 const myInputs = [firstName, lastName, email, password];
+const myLabels = ['First Name:', 'Last Name:', 'Email:', 'Password:'];
 
 submit.addEventListener('click', () => {
-    console.log(firstName.value);
+    let count = myInputs.length;
+
+    tableHead.innerHTML = '';
+    tableBody.innerHTML = '';
+
     for(let list in myInputs){
-        myUL.innerHTML += `<li>${myInputs[list].value}</li>`;
-        myInputs[list].value = '';
+        if(myInputs[list].value == ''){
+            count--;
+        }
+        if(count === myInputs.length){
+
+            tableHead.innerHTML += `<th>${myLabels[list]}</th>`;
+            tableBody.innerHTML += `<td>${myInputs[list].value}</td>`;
+
+            myInputs[list].value = '';
+            error.innerText = '';
+        } else {
+            error.innerText = 'Fill all the inputs before submiting';
+        }
     }
 });
